dd7ea209adfaa915ce9800493c0429569220d263
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2024-02-15 13:58:17 -0500

    Add simple buttons

--- ButtonSwiftUI/ContentView.swift
+++ ButtonSwiftUI/ContentView.swift
@@ -10,12 +10,41 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                debugPrint("Hola terricola")
+            } label: {
+                Text("Tap me".uppercased())
+                    .padding()
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .background(.red)
+                    .cornerRadius(12)
+                    .shadow(radius: 12)
+            }
+            
+            Divider()
+            
+            Button("Tap me") {
+                debugPrint("Hola marciano")
+            }
+            
+            Divider()
+            
+            Button(action: {
+                debugPrint("Like...")
+            },label: {
+                Circle()
+                    .fill(.blue)
+                    .frame(width: 200, height: 200)
+                    .shadow(color: .yellow, radius: 20)
+                    .overlay(
+                        Image(systemName: "hand.thumbsup.fill")
+                            .foregroundColor(.white)
+                            .font(.system(size: 70, weight: .bold))
+                    )
+            })
         }
-        .padding()
     }
 }
 

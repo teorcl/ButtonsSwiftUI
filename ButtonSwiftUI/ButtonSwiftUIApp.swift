c47ddf456c22b1d06cb8ee49803c11af2b588640
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2024-02-15 11:51:12 -0500

    first commit

---
+++ ButtonSwiftUI/ButtonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonSwiftUIApp.swift
+//  ButtonSwiftUI
+//
+//  Created by Teodoro Calle Lara on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ButtonSwiftUI
+//
+//  Created by Teodoro Calle Lara on 15/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
